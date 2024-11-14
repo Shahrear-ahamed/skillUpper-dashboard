7537b46a9c98ff01a6bf237e65aa31d986d1e7ce
Shahrear Ahamed <its.shahrear@gmail.com>
2024-11-14 19:25:34 +0600

    [FIX] remove pwa from website

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// import type { NextConfig } from "next";
-
-// // pwa
-// const withPWA = require("next-pwa")({
-//   dest: "public",
-//   disable: process.env.NODE_ENV === "development",
-//   register: true,
-//   skipWaiting: true,
-// });
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   reactStrictMode: true,
-//   swcMinify: true,
-// };
-
-// export default withPWA(nextConfig);
-
-import { NextConfig } from "next";
-import PWA from "next-pwa";
-
-const withPWA = PWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
   swcMinify: true,
-} satisfies NextConfig;
+};
 
-export default withPWA(nextConfig as any);
+export default nextConfig;
